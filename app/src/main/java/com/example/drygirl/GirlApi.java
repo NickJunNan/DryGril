06d84332df32cea3e8eb8b5ad754577a407433f8
Nick <nick@gmail.com>
2019-04-29 14:32:13 +0800

    Merge branch 'master' of https://github.com/NickJunNan/DryGril
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- app/src/main/java/com/example/drygirl/GirlApi.java
+++ app/src/main/java/com/example/drygirl/GirlApi.java
@@ -12,7 +12,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
--- app/src/main/java/com/example/drygirl/PictureLoader.java
+++ app/src/main/java/com/example/drygirl/PictureLoader.java
@@ -60,7 +60,7 @@ public class PictureLoader {
                     InputStream in = connection.getInputStream();
                     ByteArrayOutputStream out = new ByteArrayOutputStream();
                     byte[] bytes = new byte[1024];
-                    int length = -1;
+                    int length;
                     while ((length = in.read(bytes)) != -1){
                         out.write(bytes,0,length);
                     }
